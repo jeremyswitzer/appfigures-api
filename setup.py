5aa6652783f31bda516c8ac214671863803e6541
Jeremy Switzer <jeremy.switzer@gmail.com>
2012-07-11 15:54:33 -0500

    Upping version before merge.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 '''The setup and build script for the appfigures library.'''
 
 __author__ = 'jeremy.switzer@gmail.com'
-__version__ = '0.5dev'
+__version__ = '0.6dev'
 
 setup(
   name = "appfigures",
